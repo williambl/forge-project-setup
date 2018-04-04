5639dbc877a7ef0cc5c1a8bec689ce61c0fd58d9
williambl <william@williambl.com>
2018-04-04 23:59:31 +0100

    Now gets list of forge versions from multimc metadata

--- forge-project-setup.py
+++ forge-project-setup.py
@@ -1,6 +1,17 @@
 import os
+import requests
 
+forge_versions = requests.get("https://v1.meta.multimc.org/net.minecraftforge/")
+
+versions_json = forge_versions.json()
+
+version_numbers = []
+for version in versions_json["versions"]:
+    version_numbers.append(version["version"])
+
+print(version_numbers)
 project_name = input("Project name\n> ")
 os.mkdir(project_name)
 os.chdir(project_name)
+
 
