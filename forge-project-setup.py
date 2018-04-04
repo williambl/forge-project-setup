fe6dce020b30051a12d6f8f6a413a27515838d53
williambl <william@williambl.com>
2018-04-05 00:29:34 +0100

    The program now creates a download url from version numbers

--- forge-project-setup.py
+++ forge-project-setup.py
@@ -5,11 +5,17 @@ forge_versions = requests.get("https://v1.meta.multimc.org/net.minecraftforge/")
 
 versions_json = forge_versions.json()
 
-version_numbers = []
+versions = []
 for version in versions_json["versions"]:
-    version_numbers.append(version["version"])
+    versions.append(version)
 
-print(version_numbers)
+
+latest_version = versions[0]
+latest_version_mc_version = latest_version["requires"][0]["equals"]
+latest_version_number = latest_version["version"]
+
+url = "http://files.minecraftforge.net/maven/net/minecraftforge/forge/"+latest_version_mc_version+"-"+latest_version_number+"/forge-"+latest_version_mc_version+"-"+latest_version_number+"-mdk.zip"
+
 project_name = input("Project name\n> ")
 os.mkdir(project_name)
 os.chdir(project_name)
