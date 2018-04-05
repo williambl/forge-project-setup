580f8cc607dde18d3b517fa2f97ea659088bf9b2
williambl <william@williambl.com>
2018-04-05 11:17:15 +0100

    Cleaned up code

--- forge-project-setup.py
+++ forge-project-setup.py
@@ -8,35 +8,40 @@ def setup_arguments():
     parser.add_argument("project_name", help="Name of the project to create")
     parser.add_argument("-V", "--forge_version", help="Forge mdk version to download")
     parser.add_argument("-v", "--mc_version", help="Minecraft version to use. Ignored when -V is used")
-    args = parser.parse_args()
-    project_name = args.project_name
+    return parser.parse_args()
 
-setup_arguments()
+def get_forge_versions(): 
+    forge_versions = requests.get("https://v1.meta.multimc.org/net.minecraftforge/")
+    versions_json = forge_versions.json()
 
-forge_versions = requests.get("https://v1.meta.multimc.org/net.minecraftforge/")
+    versions = []
+    for version in versions_json["versions"]:
+        versions.append(version)
+    return versions
 
-versions_json = forge_versions.json()
+def download_and_extract_mdk(versions):
+    latest_version = versions[0]
+    latest_version_mc_version = latest_version["requires"][0]["equals"]
+    latest_version_number = latest_version["version"]
 
-versions = []
-for version in versions_json["versions"]:
-    versions.append(version)
+    url = "http://files.minecraftforge.net/maven/net/minecraftforge/forge/"+latest_version_mc_version+"-"+latest_version_number+"/forge-"+latest_version_mc_version+"-"+latest_version_number+"-mdk.zip"
 
+    download_zip = requests.get(url)
 
-latest_version = versions[0]
-latest_version_mc_version = latest_version["requires"][0]["equals"]
-latest_version_number = latest_version["version"]
+    with open("tmp.zip", 'wb') as fd:
+        for chunk in download_zip.iter_content(chunk_size=128):
+            fd.write(chunk)
 
-url = "http://files.minecraftforge.net/maven/net/minecraftforge/forge/"+latest_version_mc_version+"-"+latest_version_number+"/forge-"+latest_version_mc_version+"-"+latest_version_number+"-mdk.zip"
+    with zipfile.ZipFile("tmp.zip", 'r') as zip_file:
+        zip_file.extractall(".")
+        zip_file.close()
 
-os.mkdir(project_name)
-os.chdir(project_name)
+args = setup_arguments()
 
-download_zip = requests.get(url)
+versions = get_forge_versions()
 
-with open("tmp.zip", 'wb') as fd:
-    for chunk in download_zip.iter_content(chunk_size=128):
-        fd.write(chunk)
+os.mkdir(args.project_name)
+os.chdir(args.project_name)
 
-with zipfile.ZipFile("tmp.zip", 'r') as zip_file:
-    zip_file.extractall(".")
-    zip_file.close()
+download_and_extract_mdk(versions)
+
